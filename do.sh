f6bdfae86f3170c9700f142d274409f791050278
daktak <daktak@gmail.com>
2016-10-25 22:35:03 +1100

    change roomservice - fix camera

--- do.sh
+++ do.sh
@@ -2,7 +2,7 @@
 make clobber
 for n in `grep + .repo/local_manifests/device.patch | grep -v ++ | grep -v PRODUCT | grep -v @@ | grep -v device.mk | grep -v wpa | awk '{print $2}'`; do mkdir out/target/common/obj/APPS/${n}_intermediates -p; done
 
-curl --create-dirs -L -o .repo/local_manifests/roomservice.xml -O -L https://raw.githubusercontent.com/Seraph08/oneplus2_manifests/master/cm-14.0.xml
+#curl --create-dirs -L -o .repo/local_manifests/roomservice.xml -O -L https://raw.githubusercontent.com/Seraph08/oneplus2_manifests/master/cm-14.0.xml
 startday=`date +%Y%m%d`
 .repo/local_manifests/stash.sh
 repo sync --force-sync
