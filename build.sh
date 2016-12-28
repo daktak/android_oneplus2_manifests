23d8e78e18c4fe60ea123d4e2e5af17ca4afe410
Daktak <daktak@domo.ddns.net>
2016-12-29 10:01:30 +1100

    LineageOS and OP3 option

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash 
 #repo sync --force-broken --force-sync --no-clone-bundle --quiet
+dev=$1
+if [ -z ${dev} ]; then
+  dev=oneplus2
+fi
 . build/envsetup.sh
 #export PATH=/home/daktak/.gentoo/java-config-2/current-user-vm/bin:$PATH
 export USE_CCACHE=1
 prebuilts/misc/linux-x86/ccache/ccache -M 25G
 export ANDROID_CCACHE_SIZE="25G"
 export ANDROID_CCACHE_DIR="/home/user/android/.ccache"
-breakfast oneplus2
+breakfast ${dev} 
 if [ $? -ne 0 ]; then
   exit 1
 fi
@@ -15,4 +19,4 @@ mka org.cyanogenmod.platform-res -j2
 #do custom apps first
 #for n in `grep + .repo/local_manifests/enabled/device.patch | grep -v ++ | grep -v PRODUCT | grep -v @@ | grep -v device.mk | grep -v wpa | awk '{print $2}'`; do mka ${n}; done
 
-brunch oneplus2 > build.log
+brunch ${dev} > build.log
--- do.sh
+++ do.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+dev=oneplus2
 make clobber && make clean
 if [ $? -ne 0 ]; then
   exit 1
 fi
 for n in `grep + .repo/local_manifests/enabled/device.patch | grep -v ++ | grep -v PRODUCT | grep -v @@ | grep -v device.mk | grep -v wpa | awk '{print $2}'`; do mkdir out/target/common/obj/APPS/${n}_intermediates -p; done
-#curl --create-dirs -L -o .repo/local_manifests/roomservice.xml -O -L https://raw.githubusercontent.com/Seraph08/oneplus2_manifests/master/cm-14.0.xml
 startday=`date +%Y%m%d`
 .repo/local_manifests/stash.sh
-repo sync --force-sync
+repo sync --force-sync -c
 if [ $? -ne 0 ]; then
   exit 1
 fi
@@ -15,19 +15,39 @@ fi
 .repo/local_manifests/patch.sh
 rm frameworks/base/core/res/res/values/*.orig
 rm device/oneplus/oneplus2/*.orig
+rm device/oneplus/oneplus3/*.orig
 
-.repo/local_manifests/build.sh
+.repo/local_manifests/build.sh oneplus2
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-filename=out/target/product/oneplus2/cm-14.1-*-UNOFFICIAL-oneplus2.zip
-newfile=~/roms/cm-14.1-${startday}-microg-oneplus2.zip
+filename=out/target/product/${dev}/cm-14.1-*-UNOFFICIAL-${dev}.zip
+newfile=~/roms/cm-14.1-${startday}-microg-${dev}.zip
 mv ${filename} ${newfile}
 MSG=`tail -2 build.log`
-androidpn.py -t "CM 14.1 BUILD" -m "${MSG}"
+androidpn.py -t "OP2 CM 14.1 BUILD" -m "${MSG}"
 #ftppass=`cat .repo/local_manifest/ftppassword`
 pass=`cat .repo/local_manifests/afhpassword`
-#.repo/local_manifests/put.ex ${pass} uploads.androidfilehost.com daktak ${newfile}
-#lftp ftp://daktak:${ftppass}@192.168.1.92:12345 -e "cd roms; put cm-14.1-20161223-microg-oneplus2.zip;  bye"
+#lftp ftp://daktak:${ftppass}@192.168.1.92:12345 -e "cd roms; put cm-14.1-20161223-microg-${dev}.zip;  bye"
 lftp sftp://daktak:${pass}@uploads.androidfilehost.com  -e "put ${newfile}; bye"
+exit 0
+
+# rm out/target/product/oneplus2/ -rf
+# rm ../.ccache/* -rf
+# dev=oneplus3
+# patch -r - -p0 -R < .repo/local_manifests/enabled/afh.patch
+# patch -r - -p0 < .repo/local_manifests/enabled/op3/afh.patch
+# .repo/local_manifests/build.sh ${dev}
+# if [ $? -ne 0 ]; then
+#   exit 1
+# fi
+# newfile=~/roms/cm-14.1-${startday}-microg-${dev}.zip
+# filename=out/target/product/${dev}/cm-14.1-*-UNOFFICIAL-${dev}.zip
+# mv ${filename} ${newfile}
+# MSG=`tail -2 build.log`
+# androidpn.py -t "OP3 CM 14.1 BUILD" -m "${MSG}"
+# lftp sftp://daktak:${pass}@uploads.androidfilehost.com  -e "put ${newfile}; bye"
+# patch -r - -p0 -R < .repo/local_manifests/enabled/op3/afh.patch
+# patch -r - -p0 < .repo/local_manifests/enabled/afh.patch
+
