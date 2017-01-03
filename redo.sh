a46a22154ef48c2df34c7551195ae6b9c193e702
daktak <daktak@domo.ddns.net>
2017-01-03 14:24:06 +1100

    Delete redo.sh

--- redo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-repo sync --force-sync
-#repo sync --force-broken --force-sync --no-clone-bundle --quiet
-.repo/local_manifests/patch.sh
-rm frameworks/base/core/res/res/values/*.orig
-rm device/oneplus/oneplus2/*.orig
-rm frameworks/base/core/res/res/values/*.orig
-.repo/local_manifests/do.sh
