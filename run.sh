9694472129d6e6f1767329a26bec6d5244cc307d
Daktak <daktak@domo.ddns.net>
2017-01-30 23:16:10 +1100

    run all  fix

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#export JACK_SERVER_VM_ARGUMENTS="-Xmx2g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 for dev in `cat .repo/local_manifests/device-priority.txt`; do 
 .repo/local_manifests/do.sh ${dev} $1
 if [ $? -ne 0 ]; then
+  echo "do failed"
   exit 1
 fi
 rm -rf out/target/product/${dev}
